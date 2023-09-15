14f882d71012deac536d156e741a2dbd383a3c25
Sahana Koli <sahanasultanakoli@gmail.com>
2023-09-15 15:31:03 +0600

    show all course data

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,7 +1,16 @@
-const Cart = () => {
+/* eslint-disable react/jsx-key */
+/* eslint-disable react/prop-types */
+const Cart = ({selectCourse}) => {
+    console.log(selectCourse);
     return (
-        <div>
+        <div className="  ">
             
+            <h1 className="text-3xl font-medium underline ">Course Name</h1>
+            {
+                selectCourse.map((course) => (
+                    <li>{course.course_name}</li>
+                ))
+            }
         </div>
     );
 };
--- src/component/Course/Course.jsx
+++ src/component/Course/Course.jsx
@@ -1,16 +1,55 @@
+/* eslint-disable no-undef */
+
+/* eslint-disable react/jsx-key */
+import { useEffect } from "react";
+import Cart from "../Cart/Cart";
+import { useState } from "react";
+
+
 const Course = () => {
+    const [allCourse, setAllCourse] = useState([]);
+    const [selectCourse, setSelectCourse] = useState([]);
+
+
+    useEffect(() =>{
+        fetch("./data.json")
+        .then(res => res.json())
+        .then(data => setAllCourse(data))
+    }, [])
+    console.log(allCourse);
+    const handleSelectCourse = (course) =>{
+      console.log(course);
+      setSelectCourse([...selectCourse, course])
+    }
+    console.log(selectCourse);
     return (
         <div>
-            <h1 className="text-4xl text-center font-bold">Course Registration</h1>
-            <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src="https://i.ibb.co/4Pxxt3b/Rectangle-2.png" alt="Shoes" /></figure>
-            <div className="card-body">
-            <h2 className="card-title">Shoes!</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
-           </div>
-        </div>
+            <h1 className="text-4xl text-center font-bold mt-12">Course Registration</h1>
+        <div className=" flex justify-between mt-8">
+            <div className="grid grid-cols-3 gap-4">
+            {
+                allCourse.map((course) => (
+                    <div className="card w-80 bg-base-100 shadow-xl p-2">
+                <figure className=""><img src={course.image} alt="" /></figure>
+                <div className="card-body">
+                   <h2 className="card-title text-xl font-medium">{course.course_name}</h2>
+                   <p className="text-lg">{course.details}</p>
+                   <div className="flex justify-between">
+                    <p>Price: {course.price}</p>
+                    <p>Credit: {course.credit_time}hrs</p>
+                   </div>
+                   <div className="card-actions justify-end">
+                       <button onClick={() => handleSelectCourse(course)} className="btn btn-primary">Select</button>
+                   </div>
+                </div>
+            </div>
+                ))
+            }
+            </div>
+            <div>
+                <Cart selectCourse={selectCourse}></Cart>
+            </div>
+            
         </div>
         </div>
     );
