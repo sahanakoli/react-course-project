58f4b8cc2facf0f60f6ea904191244419b515e53
Sahana Koli <sahanasultanakoli@gmail.com>
2023-09-15 21:52:23 +0600

    handle select button and show course name

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,16 +1,21 @@
+/* eslint-disable no-undef */
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
-const Cart = ({selectCourse}) => {
-    console.log(selectCourse);
+const Cart = ({selectCourse, hoursRemaining, totalHours}) => {
+    
     return (
-        <div className="  ">
-            
-            <h1 className="text-3xl font-medium underline ">Course Name</h1>
+        <div className="w-72 bg-base-100 shadow-xl p-2 ml-8">
+            <h3 className="text-lg text-blue-600">Credit Hours Remaining {hoursRemaining} hr</h3>
+            <hr className="mt-4"></hr>
+            <h1 className="text-3xl font-medium mt-4 ">Course Name</h1>
             {
                 selectCourse.map((course) => (
                     <li>{course.course_name}</li>
                 ))
             }
+            <hr className="mt-4"></hr>
+            <h3 className="mt-4">Total Credit Hours:{totalHours}</h3>
+            <hr className="mt-4"></hr>
         </div>
     );
 };
--- src/component/Course/Course.jsx
+++ src/component/Course/Course.jsx
@@ -4,11 +4,16 @@
 import { useEffect } from "react";
 import Cart from "../Cart/Cart";
 import { useState } from "react";
+import {  FaDollarSign } from 'react-icons/fa';
+import { LuBookOpen  } from 'react-icons/lu';
+
 
 
 const Course = () => {
     const [allCourse, setAllCourse] = useState([]);
     const [selectCourse, setSelectCourse] = useState([]);
+    const [hoursRemaining, setHoursRemaining] = useState(0);
+    const [totalHours, setTotalHours] = useState(0);
 
 
     useEffect(() =>{
@@ -16,30 +21,57 @@ const Course = () => {
         .then(res => res.json())
         .then(data => setAllCourse(data))
     }, [])
-    console.log(allCourse);
+    
     const handleSelectCourse = (course) =>{
-      console.log(course);
-      setSelectCourse([...selectCourse, course])
+      const isCourse = selectCourse.find((item) => item.id == course.id); 
+      
+      let hours = course.credit_time;
+      if(isCourse){
+        return alert("Already Select");
+      }
+      else{
+
+        selectCourse.forEach((item) => {
+            hours = hours + item.credit_time;
+        }); 
+        const hourRemaining = 20 - hours;
+
+        if(hours > 20){
+          return alert('Sorry')
+        }
+        else{
+            setTotalHours(hours);
+            setHoursRemaining(hourRemaining);
+            setSelectCourse([...selectCourse, course]);
+        }
+      }
+      
     }
-    console.log(selectCourse);
+   
     return (
         <div>
             <h1 className="text-4xl text-center font-bold mt-12">Course Registration</h1>
-        <div className=" flex justify-between mt-8">
-            <div className="grid grid-cols-3 gap-4">
+        <div className=" flex justify-between mt-8 ">
+            <div className="grid grid-cols-3 gap-4 ">
             {
                 allCourse.map((course) => (
-                    <div className="card w-80 bg-base-100 shadow-xl p-2">
+                    <div className="card w-full bg-base-100 shadow-xl p-4">
                 <figure className=""><img src={course.image} alt="" /></figure>
                 <div className="card-body">
-                   <h2 className="card-title text-xl font-medium">{course.course_name}</h2>
-                   <p className="text-lg">{course.details}</p>
+                   <h2 className="card-title text-xl font-medium ">{course.course_name}</h2>
+                   <p className="text-lg ">{course.details}</p>
                    <div className="flex justify-between">
-                    <p>Price: {course.price}</p>
-                    <p>Credit: {course.credit_time}hrs</p>
+                    <div className="flex">
+                        <p className="mt-1"><FaDollarSign></FaDollarSign></p>
+                        <p className="ml-3 ">Price: {course.price}</p>
+                    </div>
+                    <div className="flex">
+                      <p className="mt-1"><LuBookOpen></LuBookOpen></p>
+                      <p className="ml-3">Credit: {course.credit_time}hrs</p>
+                    </div>
                    </div>
-                   <div className="card-actions justify-end">
-                       <button onClick={() => handleSelectCourse(course)} className="btn btn-primary">Select</button>
+                   <div className="card-actions justify-center ">
+                       <button onClick={() => handleSelectCourse(course)} className="btn btn-primary w-full">Select</button>
                    </div>
                 </div>
             </div>
@@ -47,7 +79,11 @@ const Course = () => {
             }
             </div>
             <div>
-                <Cart selectCourse={selectCourse}></Cart>
+                <Cart 
+                selectCourse={selectCourse}
+                hoursRemaining={hoursRemaining}
+                totalHours={totalHours}
+                ></Cart>
             </div>
             
         </div>
